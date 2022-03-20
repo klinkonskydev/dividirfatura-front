4a529dd0cc92f5e8058c767423e84dc2af797711
klinkonskydev <klinkonskydev@gmail.com>
2022-03-20 20:33:54 -0300

    feat - register page

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
+import AuthTemplate from 'templates/Auth'
+
+export default function SignUpPage() {
+  return (
+    <AuthTemplate>
+      <FormSignUp />
+    </AuthTemplate>
+  )
+}
