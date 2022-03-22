e175bab45e2c279e8dbee4b3fd2360a74b1d4432
klinkonskydev <klinkonskydev@gmail.com>
2022-03-22 08:08:11 -0300

    refactor - input change and value passed on change

--- src/components/Textbox/test.tsx
+++ src/components/Textbox/test.tsx
@@ -1,5 +1,6 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import renderWithTheme from 'utils/renderWithTheme'
+import userEvent from '@testing-library/user-event'
 
 import Textbox from '.'
 
@@ -23,5 +24,21 @@ describe('<Textbox />', () => {
       border: '1px solid #F0F0F0',
       padding: '1.6rem'
     })
+  })
+
+  it('should type in input', async () => {
+    const onInputChange = jest.fn()
+    renderWithTheme(<Textbox onInputChange={onInputChange} />)
+
+    const input = screen.getByRole('textbox')
+    const text = 'test klinkonsky'
+
+    userEvent.type(input, text)
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
+    })
+
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 })
